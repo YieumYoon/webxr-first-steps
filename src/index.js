6bb9c58b8fb2701bf7487a2d2dbfa769c98be115
YieumYoon <junsueddie@gmail.com>
2025-06-05 19:32:47 -0400

    Finished Tutorial Chapter 4

--- src/index.js
+++ src/index.js
@@ -7,11 +7,13 @@
 
 import * as THREE from 'three';
 
-// import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 // import { Text } from 'troika-three-text';
 import { XR_BUTTONS } from 'gamepad-wrapper';
 // import { gsap } from 'gsap';
 import { init } from './init.js';
+
+
 
 const bulletGeometry = new THREE.SphereGeometry(0.02);
 const bulletMaterial = new THREE.MeshStandardMaterial({ color: 'gray' });
@@ -22,8 +24,9 @@ const forwardVector = new THREE.Vector3(0, 0, -1);
 const bulletSpeed = 10;
 const bulletTimeToLive = 1;
 
-// const blasterGroup = new THREE.Group();
-// const targets = [];
+// Tutorial Chapter 4: Replacing Basic Objects with GLTF Models
+const blasterGroup = new THREE.Group();
+const targets = []; 
 
 // let score = 0;
 // const scoreText = new Text();
@@ -44,55 +47,53 @@ const bulletTimeToLive = 1;
 // }
 
 function setupScene({ scene, camera, renderer, player, controllers }) {
+	
+	// const floorGeometry = new THREE.PlaneGeometry(6, 6);
+	// const floorMaterial = new THREE.MeshStandardMaterial({ color: 'white' });
+	// const floor = new THREE.Mesh(floorGeometry, floorMaterial);
+	// scene.add(floor);
+	// floor.rotateX(-Math.PI / 2);
 
-	const floorGeometry = new THREE.PlaneGeometry(6, 6);
-	const floorMaterial = new THREE.MeshStandardMaterial({ color: 'white' });
-	const floor = new THREE.Mesh(floorGeometry, floorMaterial);
-	scene.add(floor);
-	floor.rotateX(-Math.PI / 2);
+	// const coneGeometry = new THREE.ConeGeometry(0.6, 1.5);
+	// const coneMaterial = new THREE.MeshStandardMaterial({ color: 'purple' });
+	// const cone = new THREE.Mesh(coneGeometry, coneMaterial);
+	// scene.add(cone);
+	// cone.position.set(0.4, 0.75, -1.5);
 
-	const coneGeometry = new THREE.ConeGeometry(0.6, 1.5);
-	const coneMaterial = new THREE.MeshStandardMaterial({ color: 'purple' });
-	const cone = new THREE.Mesh(coneGeometry, coneMaterial);
-	scene.add(cone);
-	cone.position.set(0.4, 0.75, -1.5);
+	// const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+	// const cubeMaterial = new THREE.MeshStandardMaterial({ color: 'orange' });
+	// const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+	// scene.add(cube);
+	// cube.position.set(-0.8, 0.5, -1.5);
+	// cube.rotateY(Math.PI / 4);
 
-	const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
-	const cubeMaterial = new THREE.MeshStandardMaterial({ color: 'orange' });
-	const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-	scene.add(cube);
-	cube.position.set(-0.8, 0.5, -1.5);
-	cube.rotateY(Math.PI / 4);
+	// const sphereGeometry = new THREE.SphereGeometry(0.4);
+	// const sphereMaterial = new THREE.MeshStandardMaterial({ color: 'red' });
+	// const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+	// scene.add(sphere);
+	// sphere.position.set(0.6, 0.4, -0.5);
+	// sphere.scale.set(1.2, 1.2, 1.2);
 
-	const sphereGeometry = new THREE.SphereGeometry(0.4);
-	const sphereMaterial = new THREE.MeshStandardMaterial({ color: 'red' });
-	const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-	scene.add(sphere);
-	sphere.position.set(0.6, 0.4, -0.5);
-	sphere.scale.set(1.2, 1.2, 1.2);
-
-	// const gltfLoader = new GLTFLoader();
-
-	// gltfLoader.load('assets/spacestation.glb', (gltf) => {
-	// 	scene.add(gltf.scene);
-	// });
-
-	// gltfLoader.load('assets/blaster.glb', (gltf) => {
-	// 	blasterGroup.add(gltf.scene);
-	// });
-
-	// gltfLoader.load('assets/target.glb', (gltf) => {
-	// 	for (let i = 0; i < 3; i++) {
-	// 		const target = gltf.scene.clone();
-	// 		target.position.set(
-	// 			Math.random() * 10 - 5,
-	// 			i * 2 + 1,
-	// 			-Math.random() * 5 - 5,
-	// 		);
-	// 		scene.add(target);
-	// 		targets.push(target);
-	// 	}
-	// });
+	// Tutorial Chapter 4: Replacing Basic Objects with GLTF Models
+	const gltfLoader = new GLTFLoader();
+	gltfLoader.load('assets/spacestation.glb', (gltf) => {
+		scene.add(gltf.scene);
+	});
+	gltfLoader.load('assets/blaster.glb', (gltf) => {
+		blasterGroup.add(gltf.scene);
+	});
+	gltfLoader.load('assets/target.glb', (gltf) => {
+		for (let i = 0; i < 3; i++) {
+			const target = gltf.scene.clone();
+			target.position.set(
+				Math.random() * 10 - 5,
+				i * 2 + 1,
+				-Math.random() * 5 - 5,
+			);
+			scene.add(target);
+			targets.push(target);
+		}
+	});
 
 	// scene.add(scoreText);
 	// scoreText.position.set(0, 0.67, -1.44);
@@ -119,21 +120,31 @@ function setupScene({ scene, camera, renderer, player, controllers }) {
 
 function onFrame( delta, time, { scene, camera, renderer, player, controllers }) {
 	if (controllers.right) {
-		const {gamepad, raySpace} = controllers.right;
-		if (gamepad.getButtonClick(XR_BUTTONS.TRIGGER)) {
-			const bullet = bulletPrototype.clone();
-			scene.add(bullet);
-			raySpace.getWorldPosition(bullet.position);
-			raySpace.getWorldQuaternion(bullet.quaternion);
+		visib
 
-			const directionVector = forwardVector
-				.clone()
-				.applyQuaternion(bullet.quaternion);
-			bullet.userData = {
-				velocity: directionVector.multiplyScalar(bulletSpeed),
-				timeToLive: bulletTimeToLive,
-			};
-			bullets[bullet.uuid] = bullet;
+		// Chapter 4: Attaching the Blaster to the Controller
+		if (!raySpace.children.includes(blasterGroup)) {
+			raySpace.add(blasterGroup);
+			mesh.visible = false; // Hide the default controller model
+		}
+
+		if (gamepad.getButtonClick(XR_BUTTONS.TRIGGER)) {
+			const bulletPrototype = blasterGroup.getObjectByName('bullet');
+				if (bulletPrototype) {
+				const bullet = bulletPrototype.clone();
+				scene.add(bullet);
+				raySpace.getWorldPosition(bullet.position);
+				raySpace.getWorldQuaternion(bullet.quaternion);
+
+				const directionVector = forwardVector
+					.clone()
+					.applyQuaternion(bullet.quaternion);
+				bullet.userData = {
+					velocity: directionVector.multiplyScalar(bulletSpeed),
+					timeToLive: bulletTimeToLive,
+				};
+				bullets[bullet.uuid] = bullet;
+				}
 		}
 
 		Object.values(bullets).forEach((bullet) => {
